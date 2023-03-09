d523af02c73cf67aefa631c5ecf0c3632234bc2b
Namnhpd07355 <127273069+Namnhpd07355@users.noreply.github.com>
2023-03-09 18:36:37 +0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+let a = prompt("Moi ban nhap so nguyen a:");        //nhập số nguyên từ bàn phím
+let a1 = parseInt(a);                               //chuyển chuỗi sang số nguyên
+let b = prompt("Moi ban nhap so nguyen b:");
+let b1 = parseInt(b);
+// C1: let c = a1 + b1;
+let c = parseInt(a1) + parseInt(b1);                //C2
+document.write(c);                                  //In ra màn hình web
+console.log(c);                                     //In ra console
